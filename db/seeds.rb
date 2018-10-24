ebd15b9dec67fd2fe534a915828fe00e624f2020
valerian.m <valerian24@hotmail.fr>
2018-10-24 18:55:17 +0200

    Exercice Terminé

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+classes = ["Sixième", "Cinquième", "Quatrième", "Troisième"]
+#
+classes.each do |sc|
+  cl = SchoolClass.create!(name: sc)
+end
+
+
+require 'faker'
+
+100.times do
+  student = Student.create!(first_name: Faker::DragonBall.character, last_name: Faker::Music.instrument, school_class_id: Random.new.rand(1..4))
+end
